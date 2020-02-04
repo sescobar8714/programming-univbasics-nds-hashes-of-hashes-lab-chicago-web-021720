08537ee832063bdb375ebc49b182ab74ebf0baf4
Samuel Escobar <sescobar8714@gmail.com>
2020-02-04 18:00:15 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -34,17 +34,12 @@ def naming_system
 
 new_hash = {
   PORTION_1 = {
-  :label=>"Kingdom",
-  :sub_category=>{
-  :label=>"Phylum",
-    :sub_category=>{
-    :label=>"Class"
-  }
-}
-},
+  },
 
 PORTION_2 = {
   :label=>"Order"
+  :sub_category=>{    
+  }
 },
 
 PORTION_3 = {
